8167f0137af2092dfc3d4daaef65b1a386881c06
Pål Grønås Drange <PGDR@equinor.com>
2019-09-05 14:26:23 +0200

    Updates

---
+++ precache-manifest.02eea2fb8d3189e32fb3d93d3e3d3b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9818ef8026a02feff5a53545fb4cf70d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7278783bcd75636ad8d8",
+    "url": "/static/css/main.afb4acdf.chunk.css"
+  },
+  {
+    "revision": "fae715cced55ee24af8b",
+    "url": "/static/js/2.e0fa2cbd.chunk.js"
+  },
+  {
+    "revision": "7278783bcd75636ad8d8",
+    "url": "/static/js/main.54626811.chunk.js"
+  },
+  {
+    "revision": "7e3bc2dc092328a9a70c",
+    "url": "/static/js/runtime~main.307179f9.js"
+  }
+]);
