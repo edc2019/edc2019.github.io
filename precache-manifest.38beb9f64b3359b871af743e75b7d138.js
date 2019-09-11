f6560640d36b6a7a05456b272d66e4ce90c06e10
Pål Grønås Drange <PGDR@equinor.com>
2019-09-11 16:09:52 +0200

    Updates

--- precache-manifest.38beb9f64b3359b871af743e75b7d138.js
+++ precache-manifest.38beb9f64b3359b871af743e75b7d138.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "87600e2a2bf1c71c5657a9a20601f1c4",
+    "revision": "2ced02c00f2b9e5f3e43c4b9883d97ae",
     "url": "/index.html"
   },
   {
-    "revision": "09ba8771253ccf53cc7b",
+    "revision": "8df9e981b49c823361b2",
     "url": "/static/css/main.afb4acdf.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e0fa2cbd.chunk.js"
   },
   {
-    "revision": "09ba8771253ccf53cc7b",
-    "url": "/static/js/main.9846bb8d.chunk.js"
+    "revision": "8df9e981b49c823361b2",
+    "url": "/static/js/main.14db259a.chunk.js"
   },
   {
     "revision": "7e3bc2dc092328a9a70c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa208ec235701823fd920f821b1a155c.js"
+  "/precache-manifest.38beb9f64b3359b871af743e75b7d138.js"
 );
 
 self.addEventListener('message', (event) => {
